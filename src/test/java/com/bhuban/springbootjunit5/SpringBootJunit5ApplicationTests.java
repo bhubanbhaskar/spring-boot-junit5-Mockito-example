0c30f980bb1a28678ad05b0461f5b9ccb6bc6d4b
Bhuban Bhaskar <bhuban@Bhubans-MacBook-Pro.local>
2023-06-11 21:24:49 +0530

    First commit

---
+++ src/test/java/com/bhuban/springbootjunit5/SpringBootJunit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhuban.springbootjunit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJunit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
